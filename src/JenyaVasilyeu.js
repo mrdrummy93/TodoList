06697a760cb0e90436b1640aafe1d48020128a9e
mrdrummy93 <vasya_2393@drummer.ru>
2020-02-10 00:59:21 +0300

    add StudentName component for first homework)

---
+++ src/JenyaVasilyeu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class StudentName extends React.Component {
+    render = () => {
+        return (
+          <div className="StudentName">
+              <p className="StudentNameTitle">Васильев Евгений Вадимович</p>
+          </div>
+        );
+    }
+}
+
+export default StudentName;
