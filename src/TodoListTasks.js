618824a28077afdbc1322f3b5ee2903ab6fa8f7b
mrdrummy93 <vasya_2393@drummer.ru>
2020-02-10 21:25:43 +0300

    after 2 lesson

--- src/TodoListTasks.js
+++ src/TodoListTasks.js
@@ -1,29 +1,22 @@
 import React from 'react';
+import TodoListTask from './TodoListTask';
 
 
 class TodoListTasks extends React.Component {
     render = () => {
+        let tasksElements = this.props.tasks.map((item)=>{
+            return (
+                <TodoListTask
+                    title={item.title}
+                    isDone={item.isDone}
+                    priority={item.priority}
+                />
+            )
+        });
         return (
-
                     <div className="todoList-tasks">
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={true}/>
-                            <span>CSS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={false}/>
-                            <span>JS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={false}/>
-                            <span>ReactJS</span>
-                        </div>
-                        <div className="todoList-task">
-                            <input type="checkbox" checked={true}/>
-                            <span>Patterns</span>
-                        </div>
+                        {tasksElements}
                     </div>
-
         );
     }
 }
