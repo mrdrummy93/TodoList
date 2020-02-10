1c8b3025f5c6a8d6cf4ec1cb092e13cf78cd29ae
mrdrummy93 <vasya_2393@drummer.ru>
2020-02-10 21:27:09 +0300

    after 2 lesson

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import TodoListHeader from "./TodoListHeader";
 import TodoListTasks from "./TodoListTasks";
 import TodoListFooter from "./TodoListFooter";
+import StudentName from "./JenyaVasilyeu";
 
 class App extends React.Component {
+    tasks = [
+        {title: "CSS", isDone:true, priority:"low"},
+        {title: "JS", isDone:false, priority:"medium"},
+        {title: "ReactJS", isDone:false, priority:"high"},
+        {title: "Patterns", isDone:true, priority:"medium"},
+    ];
+    filterValue = "Active";
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
                     <TodoListHeader/>
-                    <TodoListTasks/>
-                    <TodoListFooter/>
+                    <TodoListTasks tasks={this.tasks}/>
+                    <TodoListFooter filterValue={this.filterValue}/>
+                    <StudentName/>
                 </div>
             </div>
         );
